a853067be4eb116f7714a1445a310ad28b4fd004
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-11-01 00:47:34 +0100

    [Laboratory 4] Create task 4

--- lab04/src/t4/Main.java
+++ lab04/src/t4/Main.java
@@ -1,7 +1,59 @@
 package t4;
 
+import common.AbstractDiningPhilosophers;
+import common.AbstractPhilosopher;
+import common.Fork;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+class Philosopher extends AbstractPhilosopher {
+    public Philosopher(
+            final int place,
+            final Fork leftFork,
+            final Fork rightFork,
+            final int iterations
+    ) {
+        super(place, leftFork, rightFork, iterations);
+    }
+
+    @Override
+    protected void eat() {
+        if (ThreadLocalRandom.current().nextBoolean()) {
+            synchronized (rightFork) {
+                log("Taken right fork.");
+                synchronized (leftFork) {
+                    log("Taken left fork.");
+                }
+            }
+        }
+        else {
+            synchronized (leftFork) {
+                log("Taken left fork.");
+                synchronized (rightFork) {
+                    log("Taken right fork.");
+                }
+            }
+        }
+    }
+}
+
+class DiningPhilosophers extends AbstractDiningPhilosophers {
+    public DiningPhilosophers(final int philosophersNumber, final int iterations) {
+        super(philosophersNumber, iterations);
+    }
+
+    @Override
+    public void simulate() {
+        for (int i = 0; i < philosophersNumber; i++) {
+            Philosopher philosopher = new Philosopher(i, forks[i], forks[(i + 1) % philosophersNumber], iterations);
+            philosopher.start();
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DiningPhilosophers simulation = new DiningPhilosophers(10, 128);
+        simulation.simulate();
     }
 }
