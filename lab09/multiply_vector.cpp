d47cd149a6dc5ec64cb74f42678826dbb8cd24cc
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-12-05 19:11:22 +0100

    [Laboratory 10] Finish task 1

---
+++ lab09/multiply_vector.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+double * multiply_scalar(double *, const double);
+double multiply_vector(const double *, const double *);
+
+int main() {
+  std::cout << "Vector multiplication" << std::endl;
+
+  return 0;
+}
+
+double * multiply_scalar(double *vector, const double scalar) {
+  const int N = sizeof(vector) / sizeof(double);
+
+  #pragma omp parallel for
+  {
+    for (int i = 0; i < N; i++) {
+      vector[i] *= scalar;
+    }
+  }
+
+  return vector;
+}
+
+double multiply_vector(const double *vector1, const double *vector2) {
+  const int N = sizeof(vector1) / sizeof(double),
+            M = sizeof(vector2) / sizeof(double);
+
+  if (N != M) {
+    return 0;
+  }
+
+  double result;
+
+  #pragma omp parallel for
+  {
+    for (int i = 0; i < N; i++) {
+      result += vector1[i] * vector2[i];
+    }
+  }
+
+  return result;
+}
