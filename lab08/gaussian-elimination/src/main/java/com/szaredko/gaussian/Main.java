7769f04dd9bdb47031ac8baead3c48c5b873fc7f
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-12-25 20:06:38 +0100

    [Laboratory 8] Add algorithm to create an identity matrix from Gaussian elimination

--- lab08/gaussian-elimination/src/main/java/com/szaredko/gaussian/Main.java
+++ lab08/gaussian-elimination/src/main/java/com/szaredko/gaussian/Main.java
@@ -5,9 +5,7 @@ import com.szaredko.gaussian.operation.Operation;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Main {
     private static int n;
@@ -15,13 +13,25 @@ public class Main {
     private static double[][] mMatrix;
     private static double[][] nMatrix;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         matrix  = File.readTest();
         n       = matrix.length;
         mMatrix = new double[n][n + 1];
         nMatrix = new double[n][n + 1];
 
         List<List<Operation>> operations = File.readFoataNF();
+        initOperations(operations);
+
+        eliminateGauss(operations);
+        finishElimination();
+
+        for(double[] m : matrix){
+            System.out.println(Arrays.toString(m));
+        }
+
+    }
+
+    private static void initOperations(List<List<Operation>> operations) {
         for (List<Operation> groupOfOperations : operations) {
             for (Operation operation : groupOfOperations) {
                 operation.setMatrix(matrix);
@@ -29,17 +39,36 @@ public class Main {
                 operation.setNMatrix(nMatrix);
             }
         }
+    }
 
+    private static void eliminateGauss(
+        List<List<Operation>> operations
+    ) throws InterruptedException, ExecutionException {
         ExecutorService executorService = Executors.newCachedThreadPool();
         for (List<Operation> groupOfOperations : operations) {
-            executorService.invokeAll(groupOfOperations);
-            executorService.awaitTermination(2500, TimeUnit.MILLISECONDS);
+            List<Future<double[][]>> futures = executorService.invokeAll(groupOfOperations);
+            // Wait for all futures to finish.
+            for (Future<double[][]> future : futures) {
+               future.get(); // Don't need to store the result.
+            }
         }
-        executorService.shutdown();
+        executorService.shutdownNow();
+    }
 
-        for(double[] m : matrix){
-            System.out.println(Arrays.toString(m));
+    private static void finishElimination() {
+        for (int i = n - 1; i >= 0; i--) {
+            double m;
+
+            m = matrix[i][i];
+            matrix[i][i] /= m;
+            matrix[i][n] /= m;
+
+            for (int i1 = i - 1; i1 >= 0; i1--) {
+                m = matrix[i1][i];
+                for (int j1 = n; j1 > i1; j1--) {
+                    matrix[i1][j1] -= m * matrix[i][j1];
+                }
+            }
         }
-
     }
 }
